8ca24291088d7af489504a2d0885700e76407413
Adam Hanson <metatrot@protonmail.com>
2025-07-10 15:45:47 -0700

    fix start script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. venv/bin/activate
 tmux has-session -t main 2>/dev/null || tmux new-session -d -s main -n inspect 'inspect view --host 0.0.0.0'
 tmux has-session -t main:server 2>/dev/null || tmux new-window -t main -n server 'sudo ./venv/bin/python server.py'
 tmux attach-session -t main
